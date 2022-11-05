d3074834e025ef8a1377a1a4b3b9408971f116c3
DuotechB9 <duotechb9@gmail.com>
2022-11-05 12:06:53 -0400

    third

--- src/test/java/com/duotify/runners/CucumberRunner.java
+++ src/test/java/com/duotify/runners/CucumberRunner.java
@@ -2,8 +2,14 @@ package com.duotify.runners;
 
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
+@CucumberOptions(
+       features = "src/test/resources"
+
+)
 @RunWith(Cucumber.class)
 public class CucumberRunner {
 }
